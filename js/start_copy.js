215170c8ac5728b6734030816493e9d2d3597991
Peace <peaceonk@gmail.com>
2022-02-10 15:59:36 +0900

    complete cloneCoding

--- js/start_copy.js
+++ js/start_copy.js
@@ -7,10 +7,12 @@ const result = document.querySelector("#result");
 
 const endPoint = 12;
 // 사용자가 버튼을 눌러서 선택할때마다 배열에 값 추가
-const select = [];
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 
-function calResult(select) {
+function calResult(/* select */) {
+ /*   알고리즘 개선을 위해서 필요없어짐
+ 
     // data.js파일에서  qnaList의 type을 보면 동물이름이 있는데 같은 동물이 여러번 나온걸로 결과를 보여줄거임
     var pointArray = [
         { name: 'mouse',    value: 0, key:0 },
@@ -37,7 +39,7 @@ function calResult(select) {
                 }
             }
         }
-    }
+    } 
 
     var resultArray = pointArray.sort(function(a,b){
         if(a.value > b.value){
@@ -56,12 +58,41 @@ function calResult(select) {
     console.log(resultArray);
     // console.log(resultLamda);
 
-    let resultword = resultArray[0].key;
+        let resultword = resultArray[0].key;
     // console.log(resultword);
 
-    // TODO: 두번째로 높았던 동물도 만들어 볼까?
-
     return resultword;
+
+*/
+    console.log(Math.max(...select));
+    
+    var result = select.indexOf(Math.max(...select));
+
+    // TODO: 두번째로 높았던 동물도 만들어 볼까?
+    // select를 솔트한 다음에 구현하면 어렵지 않을듯
+    
+    return result;
+}
+
+function setResult(){
+    let point = calResult();
+
+    const resultName = document.querySelector('.resultName');
+    resultName.innerHTML = infoList[point].name;
+
+    var resultImg = document.createElement('img');
+    const imgDiv = document.querySelector('#resultImg');
+
+    var imgURL = 'img/image-' + point + '.png';
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    //부트스트랩으로 이미지 크기 조절
+    resultImg.classList.add('img-fluid');
+
+    imgDiv.appendChild(resultImg);
+
+    const resultDesc = document.querySelector('.resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
 }
 
 function goResult(){
@@ -77,11 +108,7 @@ function goResult(){
             result.style.display = "block";
         }, 450)})
 
-    // 사용자가 한 답변 보기
-    console.log("답변 기록 : "+select);
-    // calResult(select);
-
-    setResult(select);
+    setResult();
 }
 
 function addAnswer(answerText, qIdx, idx) {
@@ -113,10 +140,12 @@ function addAnswer(answerText, qIdx, idx) {
             // children[i].style.display = 'none';
         }
         setTimeout(() => {
-            
-            // 사용자가 누른 답변이 몇번인지를 기억하기 위해서
-            select[qIdx] = idx;
-            // console.log(idx);
+            // 복잡한 알고리즘 개선을 위해 버튼을 누르는 순간 data_copy.js 파일의
+            // qnaList파일의 a.type을 int 형으로 바꿔주고 그걸index로 select 배열의 값을 증가 시키기 위해
+            var target = qnaList[qIdx].a[idx].type;
+            for(let j = 0; j < target.length; j++){
+                select[target[j]] += 1;
+            }
 
             for (let i = 0; i < children.length; i++) {
                 children[i].style.display = 'none';
@@ -171,36 +200,4 @@ function begin() {
     }, 450);
     // main.style.display = "none";
     // qna.style.display = "block";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function setResult(){
-    let point = calResult(select);
-
-    const resultName = document.querySelector('.resultName');
-    resultName.innerHTML = infoList[point].name;
-
-    var resultImg = document.createElement('img');
-    const imgDiv = document.querySelector('#resultImg');
-
-    var imgURL = 'img/image-' + point + '.png';
-    resultImg.src = imgURL;
-    resultImg.alt = point;
-
-    imgDiv.appendChild(resultImg);
-
-    const resultDesc = document.querySelector('.resultDesc');
-    resultDesc.innerHTML = infoList[point].desc;
 }
